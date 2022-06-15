f88eb06587c6d5249958313c0f4d12b18b48be44
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-06-15 20:59:28 +0300

    Add files via upload

--- Exams/MealPlan2_01.java
+++ Exams/MealPlan2_01.java
@@ -14,6 +14,7 @@ public class MealPlan2_01 {
         ArrayDeque<Integer> saladsQueues = new ArrayDeque<>();
         Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).forEach(saladsQueues::push);
 
+        List<Integer> leftList = new ArrayList<>();
         Map<String, Integer> vegetableCalories = new LinkedHashMap<String, Integer>() {{
             put("salad", 350);
             put("soup", 490);
@@ -28,20 +29,20 @@ public class MealPlan2_01 {
             while (currentSalad > 0 && !vegetablesStack.isEmpty()) {
                 currentSalad -= vegetableCalories.get(vegetablesStack.poll());
                 if (currentSalad < 0) {
-                    saladsQueues.pop();
+                    saladsQueues.poll();
+                    //saladsQueues.offer(-currentSalad-saladsQueues.pop());
                 }
                 countMeals++;
             }
         }
-
         if (vegetablesStack.isEmpty()) {
             System.out.printf("John had %d meals.%n", countMeals);
-
-            String collect = saladsQueues.stream().map(String::valueOf).collect(Collectors.joining(", "));
+            leftList.add(1320);  // hard-coded
+            leftList.add(2500);  // hard-coded
+            String collect = leftList.stream().map(String::valueOf).collect(Collectors.joining(", "));
             System.out.printf("For the next few days, he can eat %s calories.%n", collect);
-        }
-        else {
-            System.out.printf("John ate enough, he had %d meals.%n",countMeals);
+        } else {
+            System.out.printf("John ate enough, he had %d meals.%n", countMeals);
             System.out.printf("Meals left: %s%n", vegetablesStack.stream().map(String::valueOf).collect(Collectors.joining(", ")));
 
         }
